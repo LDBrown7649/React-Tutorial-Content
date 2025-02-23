40f575392d39e7e3d0ecce10de10161f088e9a02
Lachlan Brown <ldbrown7649@gmail.com>
2025-02-23 16:40:24 +1100

    Configurable Button Color

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ import Hello from './Components/Hello';
 function App() {
   return (
     <div className="App">
-      <Greet/>
-      <Welcome/>
-      <Hello/>
+      <Greet name='Bruce' heroName = "Batman" overrideColor = 'Black'>
+        <p>Child props</p>
+      </Greet>
+      <Greet name='Diana' heroName = "Wonder Woman" overrideColor = 'Yellow'>
+        <button>Action</button>
+      </Greet>
+      <Greet name='Clark' heroName = "Superman"/>
+      <Welcome name ='Hal' heroName = "Green Lantern"/>
+      <Welcome name = 'Oliver' heroName = "Green Arrow"/>
+      <Welcome name = 'Vic' heroName = "Cyborg"/>
     </div>
   );
 }
--- src/Components/Greet.js
+++ src/Components/Greet.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
-const Greet = () => <h1>Hello Lachlan</h1>
+const Greet = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+            <button style = {{backgroundColor: props.overrideColor ? props.overrideColor : "white", color:"red"}}>
+                Text
+            </button>
+            {props.children}
+        </div>
+    )
+}
 
 export default Greet
